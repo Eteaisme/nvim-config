17c4883df7c74ed2fbc1883aca1ac123fb2e4f51
Elliot <eteaisme@gmail.com>
2025-10-08 19:55:43 -0400

    Willing sacrafice for startup time...

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,21 +17,22 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     {
         "catppuccin/nvim",
+        lazy = false, -- colorscheme should load immediately
+        priority = 1000,
         config = function()
             vim.cmd.colorscheme("catppuccin-macchiato")
         end,
     },
+
     {
-        "nvim-treesitter",
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPost", "BufNewFile" },
+        build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
                 ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "cpp" },
-
                 auto_install = true,
-
-                highlight = {
-                    enable = true,
-                },
+                highlight = { enable = true },
                 incremental_selection = {
                     enable = true,
                     keymaps = {
@@ -44,9 +45,7 @@ require("lazy").setup({
                 textobjects = {
                     select = {
                         enable = true,
-
                         lookahead = true,
-
                         keymaps = {
                             ["af"] = "@function.outer",
                             ["if"] = "@function.inner",
@@ -55,9 +54,9 @@ require("lazy").setup({
                             ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
                         },
                         selection_modes = {
-                            ['@parameter.outer'] = 'v', -- charwise
-                            ['@function.outer'] = 'V', -- linewise
-                            ['@class.outer'] = '<c-v>', -- blockwise
+                            ['@parameter.outer'] = 'v',
+                            ['@function.outer'] = 'V',
+                            ['@class.outer'] = '<c-v>',
                         },
                         include_surrounding_whitespace = true,
                     },
@@ -65,76 +64,91 @@ require("lazy").setup({
             })
         end,
     },
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects"
-    },
+
+    { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
+
     {
         "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
     },
+
     {
-        "mason-org/mason.nvim",
+        "williamboman/mason.nvim",
+        cmd = { "Mason" },
         config = function()
             require("mason").setup()
         end,
-
     },
+
     {
-        "mason-org/mason-lspconfig.nvim",
+        "williamboman/mason-lspconfig.nvim",
         dependencies = { "mason.nvim" },
+        event = "BufReadPre",
         config = function()
             require("mason-lspconfig").setup({
-                vim.lsp.config("lua_ls", {
-                    settings = {
-                        Lua = {
-                            diagnostics = {
-                                globals = { "vim" }}}}})
+                ensure_installed = { "lua_ls" },
+                handlers = {
+                    function(server)
+                        require("lspconfig")[server].setup({
+                            settings = {
+                                Lua = { diagnostics = { globals = { "vim" } } },
+                            },
+                        })
+                    end,
+                },
             })
         end,
     },
+
     {
         "m4xshen/hardtime.nvim",
+        event = "VeryLazy",
         dependencies = { "MunifTanjim/nui.nvim" },
         config = function()
             require("hardtime").setup()
         end,
     },
+
     {
-        "vim-telescope/telescope.nvim",
+        "nvim-telescope/telescope.nvim",
+        cmd = "Telescope",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local actions = require("telescope.actions")
             require("telescope").setup({
                 defaults = {
                     mappings = {
-                        i = {
-                            ["<esc>"] = actions.close,
-                        },
+                        i = { ["<esc>"] = actions.close },
                     },
                 },
             })
         end,
-
     },
+
     {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        "nvim-lualine/lualine.nvim",
+        event = "VimEnter",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            require('lualine').setup()
+            require("lualine").setup()
         end,
     },
+
     {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
-    config = function()
-        require('render-markdown').setup({})
-   end,
+        "MeanderingProgrammer/render-markdown.nvim",
+        ft = { "markdown" },
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
+        config = function()
+            require("render-markdown").setup({})
+        end,
     },
+
     {
-        'stevearc/oil.nvim',
+        "stevearc/oil.nvim",
+        cmd = "Oil",
         dependencies = { { "nvim-mini/mini.icons", opts = {} } },
         config = function()
             require("oil").setup()
         end,
-},
-
+    },
 })
