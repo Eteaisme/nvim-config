f2378fd1c408fd46cb8fa0382fb9ffe6bb46f677
Elliot <eteaisme@gmail.com>
2025-10-09 12:41:17 -0400

    Fixed telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,17 @@
-return{
-        "nvim-telescope/telescope.nvim",
-        cmd = "Telescope",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("telescope").setup({
-            })
-        end
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+
+  keys = {
+    { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find files" },
+    { "<leader>fw", function() require("telescope.builtin").live_grep() end, desc = "Live grep" },
+  },
+
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        prompt_prefix = "🔍 ",
+      },
+    })
+  end,
 }
