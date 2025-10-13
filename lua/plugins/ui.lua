dc12ca6e364c3a0deb80419a6539546d0627117e
Elliot <eteaisme@gmail.com>
2025-10-13 16:07:34 -0400

    ui related plugins --> ui.lua

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-macchiato")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+}
