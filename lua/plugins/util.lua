4ec0032664da8b2057a730f0a986af832381a02a
Elliot <eteaisme@gmail.com>
2025-10-28 17:01:27 -0400

    Removed which key

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -95,18 +95,4 @@ return {
 			})
 		end,
 	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-		},
-	},
 }
